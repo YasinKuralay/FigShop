333afc8f9db7e9c92bacb9fc383f481980518fe3
Yasin Kuralay <yasinkuralay@gmail.com>
2020-10-12 21:04:59 +0200

    PART 2 - Serving & Fetching Data From Express: Initialized backend Express Server.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
 import products from '../products';
-import { propTypes } from 'react-bootstrap/esm/Image';
 
 function ProductScreen({ match }) {
     const product = products.find((p) => p._id === match.params.id);
