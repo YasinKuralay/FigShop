225623cbac4dd2add5a21aca6a9afd7d0cbbb623
Yasin Kuralay <yasinkuralay@gmail.com>
2020-10-15 23:47:58 +0200

    Saving in between, redux setup

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,11 @@
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+} from '../constants/productConstants';
+
+export const listProducts = () => async (dispatch) => {
+    try {
+        dispatch({ type: })
+    }
+};
