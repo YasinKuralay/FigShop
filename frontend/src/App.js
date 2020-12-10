672b4da9895fab797a259bd97d3b51ac28fc8884
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-10 16:04:38 +0100

    Step-1: Added images and a product array for the dummy product listings before we implement the actual database. Added HomeScreen, products are visible with image and price.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import HomeScreen from './screens/HomeScreen.js';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -9,7 +10,7 @@ export default function App() {
             <Header />
             <main className='py-3'>
                 <Container>
-                    <h1>Welcome to ProShop</h1>
+                    <HomeScreen />
                 </Container>
             </main>
             <Footer />
