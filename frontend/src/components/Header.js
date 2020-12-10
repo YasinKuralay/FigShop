6e776d07dcfcf539978402292936e0e77fd3b83a
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-10 21:21:44 +0100

    STEP-1: Added Rating Component, modified Header and Product components.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Container, NavDropown } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 export default function Header() {
     return (
