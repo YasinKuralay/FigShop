926b46a2ed48c3e529cdaa9bbb9c4a2d4909d85f
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-11 19:41:19 +0100

    STEP 2 - SERVING AND FETCHING DATA FROM EXPRESS: Backend initialized. Moved the temporary data from the front to the backend.

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -9,7 +9,7 @@ export default function ProductScreen({ match }) {
 
     return (
         <>
-            <Link className='btn btn-dark my-3' to='/'>
+            <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
             <Row>
