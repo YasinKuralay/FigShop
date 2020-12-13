6594347f3d628b388d3d17992070f165531486ad
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-13 15:24:53 +0100

    STEP-3: Added a separate productRoutes file. Using expressAsyncHandler for error handling in routes.

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const connectDB = require('./config/db.js');
-const products = require('./data/products');
+
+const productRoutes = require('./routes/productRoutes.js');
 
 dotenv.config();
 
@@ -15,14 +16,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-app.get('/api/products/', (req, res) => {
-    res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
