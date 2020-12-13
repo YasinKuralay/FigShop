2d2d879756d86fc176d0b0d4a733ebed199906ed
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-13 13:55:05 +0100

    STEP-3: Added bcryptjs as a dependency. Will add a seeder script to seed the dummy products and users to the db.

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcrypt.js');
+
+const users = [
+    {
+        name: 'Admin User',
+        email: 'admin@example.com',
+        password: bcrypt.hashSync('123456', 10),
+        isAdmin: true,
+    },
+    {
+        name: 'John Doe',
+        email: 'john@example.com',
+        password: bcrypt.hashSync('1234567', 10),
+    },
+    {
+        name: 'Jane Doe',
+        email: 'Jane@example.com',
+        password: bcrypt.hashSync('12345678', 10),
+    },
+];
+
+module.exports = users;
