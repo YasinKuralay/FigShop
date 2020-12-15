351d952701b73d2b5d0f19c060ffb9ab35fac8b9
Yasin S. Kuralay <yasinkuralay@gmail.com>
2020-12-15 21:21:41 +0100

    STEP-4: Added constants for action names so we wont need to use quotes every time we write an action name. Added productActions.

---
+++ frontend/src/actions/productActions.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+//think of these as action creators, and the rest of the actions as just actions
+export const listProducts = () => async (dispatch) => {
+    // the `async (dispatch) =>` is redux thunk
+    try {
+        dispatch({ type: PRODUCT_LIST_REQUEST });
+
+        const { data } = await axios.get('/api/products');
+
+        dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data });
+    } catch (error) {
+        dispatch({
+            type: PRODUCT_LIST_FAIL,
+            payload:
+                error.response && error.response.data.message
+                    ? error.response.data.message
+                    : error.message,
+        });
+    }
+};
